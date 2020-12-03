971676c458ed3bec535bc1645cda36d74239588d
Eflamm <eflamm.ollivier@gmail.com>
2020-12-04 00:43:48 +0100

    get, create, delete article

--- database.js
+++ database.js
@@ -8,20 +8,69 @@ const client = new MongoClient(uri, {
     useUnifiedTopology: true
   });
 
-const run  = async () => {
+function createUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+       var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+       return v.toString(16);
+    });
+}
+
+const getArticles = async () => {
+    await client.connect();
+    const database = client.db("blog");
+    const collection = database.collection("articles");
+    const result = await collection.find({}).toArray();
+    if(result) {
+        console.log("Found " + result.length + " articles \u{1F4F0}");
+    } else {
+        console.warn("Could not find any articles \u{1F43E}");
+    }
+    return result || "Could not find any articles \u{1F43E}"; 
+}
+
+const getArticle = async (uuid) => {
+    await client.connect();
+    const database = client.db("blog");
+    const collection = database.collection("articles");
+    const result = await collection.findOne({"uuid":uuid});
+    if(result) {
+        console.log("Found an article whith the uuid : " + uuid + " \u{1F4F0}");
+    } else {
+        console.warn("Could not find any articles \u{1F43E}");
+    }
+    return result || "Could not find any articles \u{1F43E}"; 
+}
+
+const createArticle  = async (article) => {
+    article.uuid = createUUID();
+    let result = {};
     try {
         await client.connect();
-
         const database = client.db("blog");
         const collection = database.collection("articles");
-
-        const query = {title: "Hello world"};
-        const article = await collection.findOne(query);
-
-        console.log(article);
+        result = await collection.insertOne(article);
     } finally {
         await client.close();
+        if(result) {
+            console.log("Created the article with the UUID : " + article.uuid + "\u{1F5DE}");
+        } else {
+            console.warn("Could not create the article with the UUID : " + article.uuid + "\u{1F43E}");
+        }
+        return result.ops || "Could not create the article with the UUID : " + article.uuid + "\u{1F43E}";
     }
 }
 
-module.exports =  { run };
+const deleteArticle = async (uuid) => {
+    await client.connect();
+    const database = client.db("blog");
+    const collection = database.collection("articles");
+    const result = await collection.deleteOne({"uuid":uuid});
+    if(result) {
+        console.log("Deleted an article whith the uuid : " + uuid + " \u{1F4F0}");
+    } else {
+        console.warn("Could not find any articles \u{1F43E}");
+    }
+    return result || "Could not find any articles \u{1F43E}"; 
+}
+
+module.exports =  { getArticles, getArticle, createArticle, deleteArticle };
--- index.js
+++ index.js
@@ -1,25 +1,39 @@
 const express = require('express');
-const {run} = require('./database.js');
+const bodyParser = require('body-parser');
+const { getArticles, getArticle, createArticle, deleteArticle} = require('./database.js');
 
 const app = express();
 
 app.listen(3000, () => {
     console.log("Server listening on port 3000");
 });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const articles = [
-    {
-        uuid: "1234",
-        title: "Hello wordl",
-        body: "lorem ipsum"
-    }
-];
-
-app.get("/articles", (req, res) => {
-    res.json(articles);
+app.get("/articles", async (req, res) => {
+    console.log("GET /articles \u{1F4E8}");
+    const result = await getArticles();
+    res.json(result);
+    console.log("GET /articles \u{1F4E6}");
 });
-app.post("/articles", (req, res) => {
-    res.send("POST request");
+app.get("/articles/:uuid", async (req, res) => {
+    const uuid = req.params.uuid;
+    console.log("GET /articles/"+ uuid +" \u{1F4E8}");
+    const result = await getArticle(uuid);
+    res.json(result);
+    console.log("GET /articles/"+ uuid +" \u{1F4E6}");
 });
-
-run().catch(console.dir);
+app.post("/articles", async (req, res) => {
+    console.log("POST /articles \u{1F4E8}");
+    const article = req.body;
+    const result = await createArticle(article);
+    res.json(result);
+    console.log("POST /articles \u{1F4E6}");
+});
+app.delete("/articles/:uuid", async (req, res) => {
+    const uuid = req.params.uuid;
+    console.log("DELETE /articles/"+ uuid +" \u{1F4E8}");
+    const result = await deleteArticle(uuid);
+    res.json(result);
+    console.log("DELETE /articles/"+ uuid +" \u{1F4E6}");
+});
